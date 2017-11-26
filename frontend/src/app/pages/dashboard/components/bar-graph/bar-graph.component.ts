6b8a2f108aca7ce44d36609b109a54eb75b39040
valekar <srinivas.valekar@gmail.com>
2017-11-27 01:26:06 +0530

    added stacked bar chart

--- frontend/src/app/pages/dashboard/components/bar-graph/bar-graph.component.ts
+++ frontend/src/app/pages/dashboard/components/bar-graph/bar-graph.component.ts
@@ -48,7 +48,7 @@ export class BarGraphComponent implements OnInit, AfterViewInit {
     });
   }
 
-  buildSVG(matches: Array<Match>): void {
+  buildSVG(matches: any): void {
     //    //this.host.html('');
     //  // console.log(this.host);
     //   this.svg = this.renderer.createElement('svg');
@@ -58,11 +58,9 @@ export class BarGraphComponent implements OnInit, AfterViewInit {
     //   this.renderer.setAttribute(this.svg,'background-color','blue');
     //   this.renderer.appendChild(this.host,this.svg);
 
-    let margin = { top: 20, right: 20, bottom: 20, left: 40 },
+    let margin = { top: 80, right: 20, bottom: 20, left: 40 },
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
-
-    let rectWidth = 60;
 
     this.host.html('');
     this.svg = this.host.append('svg')
@@ -70,53 +68,91 @@ export class BarGraphComponent implements OnInit, AfterViewInit {
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+      "translate(" + margin.left + "," + (margin.top - 20) + ")");
 
     let matchesLength = matches.length;
 
-    if(matchesLength == 1){
-      width = width/7;
+    if (matchesLength == 1) {
+      width = width / 7;
     }
-    else if(matchesLength == 2){
-      width = width/6;
+    else if (matchesLength == 2) {
+      width = width / 4;
     }
-    else if(matchesLength == 3){
-      width = width/5;
+    else if (matchesLength == 3) {
+      width = width / 3;
     }
-    else if(matchesLength == 4){
-      width = width/4;
+    else if (matchesLength == 4) {
+      width = width / 3;
     }
-    else if(matchesLength == 5){
-      width = width/3;
+    else if (matchesLength == 5) {
+      width = width / 2;
     }
-    else if(matchesLength == 6){
-      width = width/2;
+    else if (matchesLength == 6) {
+      width = width / 2;
     }
-    else if(matchesLength == 7){
-      width = width/1;
+    else if (matchesLength == 7) {
+      width = width / 1;
     }
-    
-      
+
+
+    let keys = ['win','draw','lose'];
+
+    let color = d3.scaleOrdinal(d3.schemeCategory20);
+    let stack = d3.stack()
+      .keys(keys)
+      .order(d3.stackOrderNone)
+      .offset(d3.stackOffsetNone);
+
+    let layers = stack(<any>matches);
+    console.log(layers);
+    matches.sort(function (a, b) { return b.total - a.total; });
+
     // set the ranges
-    let x = d3.scaleBand()
-      .range([0, width])
-      .padding(0.1);
-    let y = d3.scaleLinear()
-      .range([height, 0]);
+    let x = d3.scaleBand().range([0, width]).padding(0.1);
+    let y = d3.scaleLinear().range([height, 0]);
 
     // Scale the range of the data in the domains
     x.domain(matches.map((d) => { return d.season; }));
-    y.domain([0, d3.max(matches, (d) => { return +d.total_matches; })]);
+    y.domain([0, d3.max(layers[layers.length - 1], function (d) { return d[0] + d[1]; })]).nice();
 
-    // append the rectangles for the bar chart
-    this.svg.selectAll(".bar")
-      .data(matches)
+    var div = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+    let layer = this.svg.selectAll(".layer")
+      .data(layers)
+      .enter().append("g")
+      .attr("class", "layer")
+      .style("fill", function (d, i) { return color(i); });
+
+    let rect = layer.selectAll("rect")
+      .data(function (d) { return d; })
       .enter().append("rect")
-      .style("fill", "steelblue")
-      .attr("x", (d) => { return x(d.season); })
+      .attr("x", (d) => { return x(d.data.season); })
+      .attr("y", function (d) { return y(d[1]); })
+      .attr("height", function (d) { return y(d[0]) - y(d[1]); })
       .attr("width", x.bandwidth())
-      .attr("y", function (d) { return y(d.total_matches); })
-      .attr("height", function (d) { return height - y(d.total_matches); });
+      .on('mouseover', function (d) {
+        div.transition()
+          .duration(200)
+          .style("opacity", .9);
+        div.html(`<div style="color:white;background-color:black;"> 
+                  Season : ${d.data.season}<br>
+                  Total Wins : ${d.data.win}<br> 
+                  Total Losses: ${d.data.lose} <br> 
+                  Total Draws: ${d.data.draw}</div>`)
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+      })
+
+
+    layer.selectAll("text").data(function (d) { return d }).enter().append("text")
+      //.text(function (d) { return d3.format(".3s")(((d[1] - d[0])/+d.data.total_matches)*100) + "%"; })
+      .text(function (d) { return d3.format(".3s")(((d[1] - d[0])/+d.data.total_matches)*100) + "%"; })
+      .attr("y", function (d) { return y(d[1]) + (y(d[0]) - y(d[1])) / 2; })
+      .attr("x", function (d) { return x(d.data.season)+x.bandwidth()/4 })
+      .style("fill", '#ffffff');
+
 
     // add the x Axis
     this.svg.append("g")
@@ -127,13 +163,45 @@ export class BarGraphComponent implements OnInit, AfterViewInit {
     this.svg.append("g")
       .call(d3.axisLeft(y));
 
-      this.svg.append("text")
+    this.svg.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left)
-      .attr("x", 0 - (height / 2)-2)
+      .attr("x", 0 - (height / 2) - 2)
       .attr("dy", "1em")
       .style("text-anchor", "middle")
-      .text("Total Matches");  
+      .text("Total Matches");
+
+    this.svg.append("text")
+      .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+      .attr("transform", "translate(" + (width / 2) + "," + ((height) + margin.top / 2) + ")")  // centre below axis
+      .text("Years");
+
+
+      var legendRectSize = 18;                                  
+      var legendSpacing = 4;                                    
+
+      let legend =  this.svg.selectAll('.legend').data(color.domain()).enter().append('g')
+        .attr('class','legend')
+        .attr('transform', function(d, i) {                     
+          var height = legendRectSize + legendSpacing;          
+          var offset =  height * color.domain().length / 2;     
+          var horz = 10 * legendRectSize;                       
+          var vert = i * height ;                       
+          return 'translate(' + horz + ',' + vert + ')';        
+        }); 
+
+
+        legend.append('rect')                                   
+        .attr('width', legendRectSize)                          
+        .attr('height', legendRectSize)                         
+        .style('fill', color)                                   
+        .style('stroke', color);  
+
+        legend.append('text')                                    
+        .attr('x', legendRectSize + legendSpacing)              
+        .attr('y', legendRectSize - legendSpacing)              
+        .text(function(d) { return keys[d]; }); 
   }
+
 
 }
